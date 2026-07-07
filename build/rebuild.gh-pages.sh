8b2bc8a0cf8232a7c279b002b4f41e4b28cba631
mk-pmb <mk-pmb@users.noreply.github.com>
2026-07-07 17:27:33 +0200

    To publish gh-pages, use ghpages-autodist-bundles from git-util-pmb.

--- build/rebuild.gh-pages.sh
+++ build/rebuild.gh-pages.sh
@@ -19,25 +19,7 @@ function rebuild_dist () {
   git status --short | grep . && return 4$(echo E: 'Worktree is unclean.' >&2)
 
   ./rebuild.sh || return $?
-  cd .. || return $?
-
-  local GHP_WT="tmp.gh-pages.$EPOCHSECONDS.$RANDOM"
-  git worktree add "$GHP_WT" || return $?
-  cd -- "$GHP_WT" || return $?
-
-  local DEST_BRANCH='gh-pages'
-  local ON_DEST="on the $DEST_BRANCH branch"
-  sed -re '/^# .* only '"$ON_DEST"':$/,/^#/s~^/~# &~' \
-    -i dist/.gitignore || return $?
-  git commit dist -m "Adjust dist/.gitignore for use $ON_DEST." || return $?
-  cp --verbose --target-directory=dist/ -- ../dist/[a-z]* || return $?
-  git add dist || return $?
-  git commit -m "Add extra dist files $ON_DEST." || return $?
-  git push origin HEAD:"$DEST_BRANCH" --force || return $?
-
-  cd .. || return $?
-  git worktree remove --force "$GHP_WT" || return $?
-  git branch -D "$GHP_WT" || return $?
+  ghpages-autodist-bundles || return $? # from git-util-pmb
 }
 
 
